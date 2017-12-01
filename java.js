bb20fa64e1afc8416006e1182d346d7235dff5fd
slynes <slynes@stevens.edu>
2017-12-01 00:03:58 -0500

    change button click to smooth scroll down the page

--- java.js
+++ java.js
@@ -1,3 +1,11 @@
 setTimeout(function showButton() {
 	$('button').fadeIn(2000);
 }, 2000);
+
+$(document).ready(function (){
+	$("button").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#moreinfo").offset().top
+		}, 2000);
+	});
+});
