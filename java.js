e2033341f4ebce58ff72441ab48d4085c0f29653
slynes <slynes@stevens.edu>
2017-12-10 00:35:45 -0500

    added Stevens portion to the page

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
 setTimeout(function showButton() {
-	$('button').fadeIn(1500);
+	$('#aboutButton').fadeIn(1500);
 }, 2000);
 
 //function for the About button to seamlessly scroll down to about section on click
 $(document).ready(function (){
-	$("button").click(function (){
+	$("#aboutButton").click(function (){
 		$('html, body').animate({
-			scrollTop: $("#moreinfo").offset().top
+			scrollTop: $("#moreinfo")[0].scrollHeight
 		}, 2000);
 	});
 });
@@ -15,7 +15,16 @@ $(document).ready(function (){
 $(document).ready(function (){
 	$("#about").click(function (){
 		$('html, body').animate({
-			scrollTop: $("#moreinfo").offset().top
+			scrollTop: $("#moreinfo")[0].scrollHeight
+		}, 2000);
+	});
+});
+
+//function for the arrow at bottom of more info div to go down to Stevens div
+$(document).ready(function (){
+	$(".arrow-down").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#stevens").offset().top
 		}, 2000);
 	});
 });
@@ -29,4 +38,25 @@ $(document).ready(function() {
 $(document).ready(function() {
 	sr.reveal('.img-thumbnail', { duration: 1000 });
 });
+
+setTimeout($(document).ready(function() {
+	sr.reveal('.arrow-down', { duration: 1000 });
+}), 3000);
+
+$(document).ready(function() {
+	sr.reveal('#stevensInfo', { duration: 1000 });
+});
+
+$(document).ready(function() {
+	sr.reveal('#stevensButton', { duration: 1000 });
+});
+
+$(document).ready(function() {
+	sr.reveal('#stevensClassButton', { duration: 1000 });
+});
+
+$(document).ready(function() {
+	sr.reveal('.img-thumbnail', { duration: 1000 });
+});
+
 
