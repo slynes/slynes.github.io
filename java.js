2e74d93d20660c94bcfa2c2bb8d3464e50be20ac
slynes <slynes@stevens.edu>
2018-01-07 21:09:41 -0500

    Big league changes to the site

--- java.js
+++ java.js
@@ -1,62 +1,66 @@
-setTimeout(function showButton() {
-	$('#aboutButton').fadeIn(1500);
-}, 2000);
-
-//function for the About button to seamlessly scroll down to about section on click
+//all functions that wil happen when the document is created
 $(document).ready(function (){
+
+	setTimeout(function showButton() {
+		$('#aboutButton').fadeIn(1500);
+	}, 2000);
+
 	$("#aboutButton").click(function (){
 		$('html, body').animate({
 			scrollTop: $("#moreinfo")[0].scrollHeight
 		}, 2000);
 	});
-});
 
-//function for the About Nav-bar button to seamlessly scroll down to about section on click
-$(document).ready(function (){
 	$("#about").click(function (){
 		$('html, body').animate({
 			scrollTop: $("#moreinfo")[0].scrollHeight
 		}, 2000);
 	});
-});
 
-//function for the arrow at bottom of more info div to go down to Stevens div hi
-$(document).ready(function (){
-	$(".arrow-down").click(function (){
+	$("#contact").click(function (){
 		$('html, body').animate({
-			scrollTop: $("#stevens").offset().top
+			scrollTop: $("#socialmedia").offset().top
 		}, 2000);
 	});
-});
 
-window.sr = ScrollReveal();
+	var heightIncreaser = 0;
+	if(screen.width > 1824) {
+		// Code for bigger screens...
+		heightIncreaser = 680;
+	  } else {
+		heightIncreaser = 475;
+	  }
 
-$(document).ready(function() {
+	$("#arrow1").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#stevens")[0].scrollHeight + heightIncreaser
+		}, 2000);
+	});
+
+	$("#arrow2").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#socialmedia").offset().top
+		}, 2000);
+	});
+
+	window.sr = ScrollReveal();
+
 	sr.reveal('#idBio', { duration: 1000 });
-});
 
-$(document).ready(function() {
 	sr.reveal('.img-thumbnail', { duration: 1000 });
-});
 
-setTimeout($(document).ready(function() {
-	sr.reveal('.arrow-down', { duration: 1000 });
-}), 3000);
+	setTimeout(
+		sr.reveal('.arrow-down', { duration: 1000 }) , 3000);
 
-$(document).ready(function() {
 	sr.reveal('#stevensInfo', { duration: 1000 });
-});
 
-$(document).ready(function() {
 	sr.reveal('#stevensButton', { duration: 1000 });
-});
 
-$(document).ready(function() {
 	sr.reveal('#stevensClassButton', { duration: 1000 });
+
+	sr.reveal('#infocolumn', { duration: 1000 });
+
 });
 
-$(document).ready(function() {
-	sr.reveal('.img-thumbnail', { duration: 1000 });
-});
 
 
